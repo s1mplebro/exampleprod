7c4e8fe9e5629a741413c60c6b1d65c837859d25
zaliv17 <gulomovnodir97@gmail.com>
2022-05-25 22:08:34 +0500

    perevod s sayta na sayt django

--- thisisprod/exm_app/urls.py
+++ thisisprod/exm_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import HomePageView,PageView
+from .views import HomePageView,PageView,aPageViewAbout
 
 urlpatterns = [
 path('',HomePageView.as_view(),name='index'),
 path('domoy/',PageView.as_view(),name='home'),
-
-#path('',HomePageView.as_view(),name='base')
+path('aboutus/',aPageViewAbout.as_view(),name='about'),
 ]
--- thisisprod/exm_app/views.py
+++ thisisprod/exm_app/views.py
@@ -3,7 +3,10 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
 	template_name = 'index.html'
-	#template_name = 'base.html'
+	
 
 class PageView(TemplateView):
 	template_name = 'home.html'
+
+class aPageViewAbout(TemplateView):
+	template_name = 'about.html'
